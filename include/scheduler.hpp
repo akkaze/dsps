b470914ea062b0d54170c3ac926f700d740227ba
zak-git <zhengankun@163.com>
2017-06-09 16:50:37 +0800

    add connect function

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -1,12 +1,55 @@
 #include <string>
 #include <iostream>
+#include <vector>
 #include <chrono>
 
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
 
+#include "base.hpp"
+#include "actions.hpp"
+
 using namespace std;
 using namespace std::chrono;
 using namespace caf;
 
-
+//struct for scheduler internal state
+struct schduler_sate {
+    vector<strong_actor_ptr> current_servers;
+    vector<strong_actor_ptr> current_workers;
+};
+class scheduler : stateful_actor<scheduler_state> {
+public:
+    scheduler(actor_config& cfg) : event_based_actor(cfg) {
+       role_ = role::scheduler; 
+       // nop
+    }
+    behavior_type recieving() override {
+    static void connecting(stateful_actor<scheduler_state>* self,
+        const std::string& host, uint16_t port,
+        const node_role& role) {
+            auto mm = self->system().middleman().actor_handle();
+            self->request(mm, infinite, connect_atom::value, host, port).await(
+            [=](const node_id&, strong_actor_ptr client,
+                const std::set<std::string>& ifs) {
+            if (!client) {
+                 aout(self) << R"(*** no client found at ")" << host << R"(":)"
+                        << port << endl;
+                return;
+            }
+            if (!ifs.empty()) {
+                aout(self) << R"(*** typed actor found at ")" << host << R"(":)"
+                        << port << ", but expected an untyped actor "<< endl;
+                return;
+            }
+            if(role == node_role::worker) {
+                self->state->current->
+        );   
+        if(role == node_role::worker) {
+            for(auto server : self->state->current_servers) {
+                
+        }     
+    }
+private:
+    node_role role_;
+};
