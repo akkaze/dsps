c5d7c4de5b6d7d4056d51ed1faf751ed20c3eca1
zak-git <zhengankun@163.com>
2017-06-15 19:02:02 +0800

    add node class

---
+++ include/node.hpp
@@ -0,0 +1,52 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+#include <string>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include "caf/all.hpp"
+#include "caf/io/all.hpp"
+
+#include "base.hpp"
+#include "actions.hpp"
+
+using namespace std;
+using namespace std::chrono;
+using namespace caf;
+
+class node : public blocking_actor {
+public: 
+    node(actor_config& cfg) : blocking_actor(cfg) {}
+    node(actor_config& cfg,const node_role& role) : blocking_actor(cfg),role_(role) {} 
+protected:
+    strong_actor_ptr connect(const std::string& host, uint16_t port) {
+        strong_actor_ptr incoming_node;
+        auto mm = this->system().middleman().actor_handle();
+        this->request(mm, infinite, connect_atom::value, host, port).receive(
+                [&](const node_id&, strong_actor_ptr node,
+                    const std::set<std::string>& ifs) {
+                if (!node) {
+                        aout(this) << R"(*** no node found at ")" << host << R"(":)"
+                               << port << endl;
+                        return;
+                    }
+                    aout(this) << R"(connected)" << endl;
+                    incoming_node = node;
+                },
+                [&](error& err) {
+                    aout(this) << this->system().render(err) << endl;
+                }
+        );
+        return incoming_node;
+    }
+    const node_role& get_role() {
+        return role_;
+    }
+    void set_role(const node_role& role) {
+        role_ = role;
+    }
+private:
+    node_role role_;
+};
+#endif
