3a819aa102197fddfcdff0299195953c7a83a9af
zak-git <zhengankun@163.com>
2017-06-15 19:13:36 +0800

    reformat some code

--- include/scheduler.hpp
+++ include/scheduler.hpp
@@ -1,3 +1,5 @@
+#ifndef SCHEDULER_HPP
+#define SCHEDULER_HPP
 #include <string>
 #include <iostream>
 #include <vector>
@@ -14,48 +16,39 @@ using namespace std::chrono;
 using namespace caf;
 
 //struct for scheduler internal state
-struct schduler_sate {
+struct scheduler_sate {
     vector<strong_actor_ptr> current_servers;
     vector<strong_actor_ptr> current_workers;
 };
-class scheduler : stateful_actor<scheduler_state> {
+
+class scheduler : node {
 public:
-    scheduler(actor_config& cfg) : event_based_actor(cfg) {
+    scheduler(actor_config& cfg) : stateful_actor(cfg) {
        role_ = role::scheduler; 
        // nop
     }
-    static behavior_type receiving() override {
-        [connect_to_opponant_atom, connect 
-            const std::string& host, uint16_t port,
-            const node_role& role] {
-            connecting(this,host,port); 
-            for(auto serv : this->state->current_servers)
-                this->delegate(actor_cast<serv>,connect,host,port,role);
-        }
+    void act() override {
+        bool running = true;
+        this->receive_while(running) (
+            //received the connect to opponant message
+            [connect_to_opponant_atom connect,
+                const std::string& host, uint16_t port,
+                const node_role& role] {
+                //firtst connect to incoming actor
+                connect(this,host,port);
+                //after connected to that actor,next,try to deliver this message to its opponant
+                if(role == node_role::worker) {
+                    for(auto server : this->state->current_servers)
+                        this->delegate(actor_cast<actor>(server),connect,host,port);
+                } else if(role == node_role::server) {
+                    for(auto work : this->state->current_workers) 
+                        this->delegate(actor_cast<actor>(worker),connect,host,port);
+                }
+            }
+        );
     }
-    //connecting routine when the scheduler receive the connect to opponant atom
-    void connecting(stateful_actor<scheduler_state>* self,
-        const std::string& host, uint16_t port,
-        const node_role& role) {
-            auto mm = self->system().middleman().actor_handle();
-            self->request(mm, infinite, connect_atom::value, host, port).await(
-            [=](const node_id&, strong_actor_ptr client,
-                const std::set<std::string>& ifs) {
-            if (!client) {
-                 aout(this) << R"(*** no client found at ")" << host << R"(":)"
-                        << port << endl;
-                return;
-            }
-            if (!ifs.empty()) {
-                aout(this) << R"(*** typed actor found at ")" << host << R"(":)"
-                        << port << ", but expected an untyped actor "<< endl;
-                return;
-            }
-            if(role == node_role::worker) {
-                this->state->current_workers.push_back(client);
-            }
-        );        
-    }
+    
 private:
-    node_role role_;
+    scheduler_state state_;
 };
+#endif
