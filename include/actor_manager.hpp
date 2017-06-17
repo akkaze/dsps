93452eb8381c232553ebe43cdf8a25418f540843
zak-git <zhengankun@163.com>
2017-06-17 20:36:37 +0800

    add actor manager singleton

--- include/actor_manager.hpp
+++ include/actor_manager.hpp
@@ -5,20 +5,22 @@ public:
     static inline actor_manager* get() {
         return _get_shared_ref(nullptr).get();
     }
-    static inline actor_system get_system() {
-        return _get_shared_ref(nullptr).system_;
+    //get actor system
+    inline actor_system system() {
+        return system_;
     }
     static inline std::shared_ptr<actor_manager> _get_shared_ref() {
         return _get_shared_ref(nullptr);
     }
 private:
-    explicit actor_manager(const actor_system* system) {
-        if(system) system_ = *system;      
+    explicit actor_manager() {
+        actor_system_config config;
+        system_.reset(new actor_system(config));
     }
-    static std::shared_ptr<actor_manager> _get_shared_ref(const actor_system* system) {
-        static std::shared_ptr<actor_manager> inst_ptr(new actor_manager(system));
+    static std::shared_ptr<actor_manager> _get_shared_ref() {   
+        static std::shared_ptr<actor_manager> inst_ptr(new actor_manager());
         return inst_ptr;
     }
-    actor_system system_;
+    std::shared_ptr<actor_system> system_;
 }
 #endif
