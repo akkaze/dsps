110d91808b3cae0e32f48e3a29cdc94b3eee6d6a
zak-git <zhengankun@163.com>
2017-06-18 16:23:48 +0800

    small fixs

--- include/actor_manager.hpp
+++ include/actor_manager.hpp
@@ -3,18 +3,19 @@
 class actor_manager {
 public:
     static inline actor_manager* get() {
-        return _get_shared_ref(nullptr).get();
+        return _get_shared_ref().get();
     }
     //get actor system
     inline actor_system system() {
         return system_;
     }
     static inline std::shared_ptr<actor_manager> _get_shared_ref() {
-        return _get_shared_ref(nullptr);
+        return _get_shared_ref();
     }
 private:
     explicit actor_manager() {
         actor_system_config config;
+        config.load<io::middleman>();
         system_.reset(new actor_system(config));
     }
     static std::shared_ptr<actor_manager> _get_shared_ref() {   
