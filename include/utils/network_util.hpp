d2af12ed7ca6d017556911de9d298f0ad87f987f
zak-git <zhengankun@163.com>
2017-07-18 10:01:16 +0800

    rewrite network util using caf

--- include/utils/network_util.hpp
+++ include/utils/network_util.hpp
@@ -1,16 +1,35 @@
 #include <string>
-#include <boost/asio.hpp>
-using boost::asio::ip::tcp;
+#include <iostream>
+#include <chrono>
+#include <tuple>
+#include <vector>
+#include "caf/all.hpp"
+#include "caf/io/all.hpp"
+using namespace std;
+using namespace std::chrono;
+using namespace caf;
+using namespace caf::io;
+using namespace caf::io::network;
 
-std::string get_local_ip() {
-    boost::asio::io_service io_service;
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(boost::asio::ip::host_name(), "");
-    tcp::resolver::iterator iter = resolver.resolve(query);
-    tcp::resolver::iterator end; // End marker.
-    while (iter != end) {
-        tcp::endpoint ep = *iter++;
-        if(!ep.address().empty())
-            return ep.address();
-    }
+std::vector<std::string>
+list_addresses(std::initializer_list<protocol> procs,
+                                   string interface,
+                                   bool include_localhost = false) {
+      std::vector<std::string> result;
+      interfaces::traverse(procs, [&](const char* name, protocol, bool lo, const char* addr) {
+        if (include_localhost || !lo && 
+            ((!interface.empty() && interface == name) || interface.empty()) )
+            result.emplace_back(addr);    
+        });
+       return result;
+}
+
+std::vector<std::string> list_addresses(protocol proc,string interface,
+      bool include_localhost= false) {
+      return list_addresses({proc}, interface,include_localhost);
+}
+
+string get_local_ip() {
+    std::vector<string> addresses = list_addresses(protocol::ipv4,"");
+    return addresses.front();
 }
