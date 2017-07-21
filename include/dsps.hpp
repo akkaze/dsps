b074f0d05aff69180c889ce780ece8e486a1aeea
zak-git <zhengankun@163.com>
2017-07-21 19:47:14 +0800

    refactor

--- include/dsps.hpp
+++ include/dsps.hpp
@@ -17,13 +17,16 @@
 using namespace std;
 using namespace caf;
 
-//#define DEFAULT_ERROR_HANLDER \
-//    [&](error& err) {  \
-//        aout(this) << this->system().render(err) << endl; \
-//    }
+#define DEFAULT_ERROR_HANLDER \
+    [&](error& err) {  \
+        aout(this) << this->system().render(err) << endl; \
+    }
+
+
 void start(size_t argc,char** argv) {
     actor_manager::get()->init(argc,argv);
     shared_ptr<config> cfg = actor_manager::get()->cfg();
+    
     if(cfg->role() == node_role::scheduler)
         std::unique_ptr<scheduler_node> scheduler(new scheduler_node(cfg)); 
     else if(cfg->role() == node_role::worker)
