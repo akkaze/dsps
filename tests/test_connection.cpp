ee8b395efa5bcb9e306f8c8f3d378ce8faa5a11a
zak-git <zhengankun@163.com>
2017-07-24 10:13:20 +0800

    new test connection

--- tests/test_connection.cpp
+++ tests/test_connection.cpp
@@ -1,7 +1,29 @@
 #include "dsps.hpp"
 
 int main(int argc,char **argv) {
-    start(argc,argv);
-    stop();
-    return 0;
+    init(argc,argv);
+    auto role = my_role();
+    switch(role) {
+        case node_role::scheduler : {
+            std::unique_ptr<scheduler_node> scheduler(new scheduler_node());
+            std::this_thread::sleep_for(seconds(2));
+            scheduler->ask_for_blocking(block_group::all_nodes);
+            scheduler->quit();
+            return 0;
+        }
+        case node_role::worker : {
+            std::unique_ptr<worker_node> worker(new worker_node());
+            std::this_thread::sleep_for(seconds(2));
+            worker->ask_for_blocking(block_group::all_nodes);
+            worker->quit(); 
+            return 0;
+        }
+        case node_role::server : {
+            std::unique_ptr<server_node> server(new server_node());
+            std::this_thread::sleep_for(seconds(2));
+            server->ask_for_blocking(block_group::all_nodes);
+            server->quit();  
+            return 0;        
+        }
+    }
 }
